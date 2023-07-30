82370549c000a876e860549330c7d4432c91753a
Gamalliel Sharon <sharongamalliel@gmail.com>
2023-07-31 00:34:09 +0700

    new sql schema

--- database/main.sql
+++ database/main.sql
@@ -1,15 +1,26 @@
-CREATE DATABASE IF NOT EXISTS art_gallery;
+CREATE DATABASE football_analysis;
 
-\c art_gallery;
+\c football_analysis;
 
-CREATE TABLE art_pieces (
+CREATE TABLE content (
     id UUID PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     image_url TEXT NOT NULL,
     description TEXT NOT NULL,
     likes INT NOT NULL DEFAULT 0,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 );
 
-INSERT INTO art_pieces(id, image_url, description) VALUES ('ba6eb330-4f7f-11eb-a2fb-67c34e9ac07c', 'https://upload.wikimedia.org/wikipedia/commons/b/b4/Lionel-Messi-Argentina-2022-FIFA-World-Cup_%28cropped%29.jpg', 'ANCARA MESSI!');
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+);
 
-INSERT INTO art_pieces(id, image_url, description) VALUES ('ba6eb330-4f7f-11eb-a2fb-67c34e9ac04e', 'https://test.com', 'STEVEN GERRARD!');
+
+INSERT INTO content (id, title, image_url, description, likes) VALUES ('ba6eb330-4f7f-11eb-a2fb-67c34e9ac07c', 'title', 'https://www.google.com', 'description', 0);
+INSERT INTO users (username, password, email) VALUES ('admin', 'admin', 'admin@admin.com')
